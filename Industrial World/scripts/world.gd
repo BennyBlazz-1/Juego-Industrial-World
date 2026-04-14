74d28f544f3e9ffd7f849ebb1783e0b5c9f2afea
Jaredalv7 <162151088+Jaredalv7@users.noreply.github.com>
2026-04-13 21:19:15 -0600

    selector

--- Industrial World/scripts/world.gd
+++ Industrial World/scripts/world.gd
@@ -3,9 +3,20 @@ extends Node2D
 @onready var building1_under_construction_map = $building1_under_construction_map
 @onready var building1_completed_map = $building1_completed_map
 
+var personajes = [
+	preload("res://scenes/man_player.tscn"),
+	#preload("res://player_woman.tscn"),
+]
+
 func _ready():
 	update_building_1_visual()
+	spawn_personaje() # 👈 NUEVO
 
 func update_building_1_visual():
 	building1_under_construction_map.visible = not GameManager.level1_passed
 	building1_completed_map.visible = GameManager.level1_passed
+
+func spawn_personaje():
+	var personaje = personajes[Global.personaje_seleccionado].instantiate()
+	personaje.position = Vector2(100, 100) # 👈 donde aparece
+	add_child(personaje)
