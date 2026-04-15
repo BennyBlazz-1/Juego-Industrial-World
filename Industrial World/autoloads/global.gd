5271aa6a6f2f8859a28bd16cd15dcd5b662b4faf
Jaredalv7 <162151088+Jaredalv7@users.noreply.github.com>
2026-04-15 11:50:52 -0600

    slectorcorregido

---
+++ Industrial World/autoloads/global.gd
@@ -0,0 +1,5 @@
+extends Node
+
+# 0 = hombre
+# 1 = mujer
+var personaje_seleccionado: int = 0
--- Industrial World/scripts/man_player.gd
+++ Industrial World/scripts/man_player.gd
@@ -1,26 +1,45 @@
 extends CharacterBody2D
 
 const speed = 200
-var current_dir = "none"
+var current_dir = "down"
 
 @onready var world_camera: Camera2D = $world_camera
 @onready var bodega_camera: Camera2D = $bodega_camera
 
+@onready var spritehombre: AnimatedSprite2D = $AnimatedSprite2D
+@onready var spritemujer: AnimatedSprite2D = $AnimatedSprite2D2
+
+var sprite_actual: AnimatedSprite2D
+
 func _ready():
 	add_to_group("player")
-	$AnimatedSprite2D.play("front_idle")
 	motion_mode = CharacterBody2D.MOTION_MODE_FLOATING
+
+	configurar_personaje()
 	call_deferred("update_current_camera")
 
-func _physics_process(delta):
+func configurar_personaje() -> void:
+	if Global.personaje_seleccionado == 0:
+		spritehombre.visible = true
+		spritemujer.visible = false
+		sprite_actual = spritehombre
+	else:
+		spritehombre.visible = false
+		spritemujer.visible = true
+		sprite_actual = spritemujer
+
+	sprite_actual.play("front_idle")
+
+func _physics_process(_delta):
 	if GameManager.is_dialogue_active:
 		velocity = Vector2.ZERO
 		play_anim(0)
 		return
-	man_player_movement(delta)
 
-func man_player_movement(_delta):
-	var input_vector: Vector2 = Vector2.ZERO
+	player_movement()
+
+func player_movement() -> void:
+	var input_vector := Vector2.ZERO
 
 	if Input.is_action_pressed("ui_right"):
 		current_dir = "right"
@@ -38,7 +57,7 @@ func man_player_movement(_delta):
 	velocity = input_vector * speed
 	move_and_slide()
 
-	var moved_distance: float = get_position_delta().length()
+	var moved_distance := get_position_delta().length()
 
 	if input_vector == Vector2.ZERO:
 		play_anim(0)
@@ -48,37 +67,37 @@ func man_player_movement(_delta):
 		velocity = Vector2.ZERO
 		play_anim(0)
 
-func play_anim(movement):
-	var dir = current_dir
-	var anim = $AnimatedSprite2D
+func play_anim(movement: int) -> void:
+	if sprite_actual == null:
+		return
 
-	if dir == "right":
-		anim.flip_h = false
+	if current_dir == "right":
+		sprite_actual.flip_h = false
 		if movement == 1:
-			anim.play("side_walk")
+			sprite_actual.play("side_walk")
 		else:
-			anim.play("side_idle")
+			sprite_actual.play("side_idle")
 
-	if dir == "left":
-		anim.flip_h = true
+	elif current_dir == "left":
+		sprite_actual.flip_h = true
 		if movement == 1:
-			anim.play("side_walk")
+			sprite_actual.play("side_walk")
 		else:
-			anim.play("side_idle")
+			sprite_actual.play("side_idle")
 
-	if dir == "down":
-		anim.flip_h = true
+	elif current_dir == "down":
+		sprite_actual.flip_h = false
 		if movement == 1:
-			anim.play("front_walk")
+			sprite_actual.play("front_walk")
 		else:
-			anim.play("front_idle")
+			sprite_actual.play("front_idle")
 
-	if dir == "up":
-		anim.flip_h = true
+	elif current_dir == "up":
+		sprite_actual.flip_h = false
 		if movement == 1:
-			anim.play("back_walk")
+			sprite_actual.play("back_walk")
 		else:
-			anim.play("back_idle")
+			sprite_actual.play("back_idle")
 
 func update_current_camera() -> void:
 	var current_scene: Node = get_tree().current_scene
