e6e4ffc3ed8f17d6e14be04e31877003860260a7
Uriel <royality73@gmail.com>
2026-04-14 22:02:42 -0600

    Lol
    Que mal

--- Industrial World/scenes/boton_interrogacion.gd
+++ Industrial World/scenes/boton_interrogacion.gd
@@ -2,11 +2,14 @@ extends Area2D
 
 @export var orden: int = 0
 
-@onready var sprite_signo = $ExclamationMark
-@onready var sonido_boton = $ExclamationMark/Boton
+@onready var sprite_signo = get_node_or_null("ExclamationMark")
+@onready var sonido_boton = get_node_or_null("ExclamationMark/Boton")
 
 var jugador_cerca: bool = false
 var completado: bool = false
+
+const DIALOGO_ERROR = "res://dialogues/nivel2_wrong.dialogue"
+const DIALOGO_COMPLETE = "res://dialogues/nivel2_complete.dialogue"
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
@@ -14,16 +17,24 @@ func _ready() -> void:
 
 	if sprite_signo:
 		sprite_signo.visible = false
+		if sprite_signo.has_method("stop"):
+			sprite_signo.stop()
 
 func _process(_delta: float) -> void:
 	if jugador_cerca and not completado and Input.is_action_just_pressed("interact"):
 		interactuar()
 
 func interactuar() -> void:
+	if GameManager.is_dialogue_active:
+		return
+
 	var fue_correcto = GameManager.completar_boton_nivel_2(name)
 
 	if fue_correcto:
 		activar_correcto()
+
+		if GameManager.nivel_2_finished:
+			mostrar_dialogo_completado()
 	else:
 		activar_incorrecto()
 
@@ -41,26 +52,47 @@ func _on_body_exited(body: Node) -> void:
 func mostrar_signo() -> void:
 	if sprite_signo:
 		sprite_signo.visible = true
+		if sprite_signo.has_method("play"):
+			sprite_signo.play()
 
 func ocultar_signo() -> void:
 	if sprite_signo and not completado:
 		sprite_signo.visible = false
+		if sprite_signo.has_method("stop"):
+			sprite_signo.stop()
 
 func activar_correcto() -> void:
 	completado = true
 
 	if sprite_signo:
 		sprite_signo.visible = true
+		if sprite_signo.has_method("play"):
+			sprite_signo.play()
 
 	if sonido_boton:
 		sonido_boton.stop()
 		sonido_boton.play()
 
-	print("Correcto:", name)
-
-	if GameManager.nivel_2_finished:
-		print("Nivel 2 completado")
-		# aquí puedes cambiar de nivel o abrir puerta
-
 func activar_incorrecto() -> void:
-	print("Incorrecto:", name)
+	mostrar_dialogo_error()
+
+func mostrar_dialogo_error() -> void:
+	GameManager.is_dialogue_active = true
+	DialogueManager.show_dialogue_balloon(load(DIALOGO_ERROR))
+	await DialogueManager.dialogue_ended
+	GameManager.is_dialogue_active = false
+
+func mostrar_dialogo_completado() -> void:
+	GameManager.is_dialogue_active = true
+	DialogueManager.show_dialogue_balloon(load(DIALOGO_COMPLETE))
+	await DialogueManager.dialogue_ended
+	GameManager.is_dialogue_active = false
+
+func reiniciar_boton() -> void:
+	completado = false
+	jugador_cerca = false
+
+	if sprite_signo:
+		sprite_signo.visible = false
+		if sprite_signo.has_method("stop"):
+			sprite_signo.stop()
