9b10d8bb7d41277f031ca5434505abe37bbc6fb8
BennyBlazz-1 <bryanlazaro4khd@gmail.com>
2026-04-16 00:41:53 -0600

    Lógica de niveles
    Completado

--- Industrial World/scripts/nivel_2.gd
+++ Industrial World/scripts/nivel_2.gd
@@ -1,16 +1,17 @@
 extends Node2D
 
-@onready var spawn_from_world: Marker2D = $spawn_from_world
+@onready var spawn_from_world: Marker2D = get_node_or_null("spawn_from_world")
 
-func _ready():
-	if not GameManager.level1_passed:
-		GameManager.reset_level1()
-
+func _ready() -> void:
 	call_deferred("place_player_at_spawn")
 
 func place_player_at_spawn() -> void:
 	var player := get_tree().get_first_node_in_group("player") as Node2D
 	if player == null:
+		return
+
+	if spawn_from_world == null:
+		push_error("No existe el nodo 'spawn_from_world' en nivel_2.tscn")
 		return
 
 	var spawn_name := GameManager.consume_next_spawn()
--- Industrial World/scripts/world.gd
+++ Industrial World/scripts/world.gd
@@ -2,45 +2,48 @@ extends Node2D
 
 @onready var building1_under_construction_map = $building1_under_construction_map
 @onready var building1_completed_map = $building1_completed_map
+
+@onready var building2_under_construction_map = $building2_under_construction_map
+@onready var building2_completed_map = $building2_completed_map
+
 @onready var spawn_from_bodega: Marker2D = $spawn_from_bodega
 @onready var spawn_from_nivel2: Marker2D = $spawn_from_nivel2
 
 @onready var man_player: Node2D = $man_player
 
 
-func _ready():
-	update_building_1_visual()
+func _ready() -> void:
+	update_buildings_visual()
 	seleccionar_personaje()
 	call_deferred("place_player_at_spawn")
 
 
-func update_building_1_visual() -> void:
+func update_buildings_visual() -> void:
+	# Edificio 1
 	building1_under_construction_map.visible = not GameManager.level1_passed
 	building1_completed_map.visible = GameManager.level1_passed
+
+	# Edificio 2
+	building2_under_construction_map.visible = not GameManager.nivel_2_passed
+	building2_completed_map.visible = GameManager.nivel_2_passed
 
 
 func seleccionar_personaje() -> void:
 	if man_player:
 		man_player.remove_from_group("player")
-	
 
 	if Global.personaje_seleccionado == 0:
-		# HOMBRE ACTIVO
-
 		if man_player:
 			man_player.visible = true
 			man_player.set_process(true)
 			man_player.set_physics_process(true)
 			man_player.add_to_group("player")
 	else:
-		# MUJER ACTIVA
 		if man_player:
 			man_player.visible = true
 			man_player.set_process(true)
 			man_player.set_physics_process(true)
 			man_player.add_to_group("player")
-
-		
 
 
 func place_player_at_spawn() -> void:
