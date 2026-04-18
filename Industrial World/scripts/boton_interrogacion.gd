a798c09c765a3bf5a297a5be3a3f9615f668c6a2
BennyBlazz-1 <bryanlazaro4khd@gmail.com>
2026-04-18 02:37:35 -0600

    Posición de los Labels

--- Industrial World/scripts/boton_interrogacion.gd
+++ Industrial World/scripts/boton_interrogacion.gd
@@ -4,6 +4,7 @@ extends Area2D
 
 @onready var sprite_signo = get_node_or_null("ExclamationMark")
 @onready var sonido_boton = get_node_or_null("ExclamationMark/Boton")
+@onready var interaction_label = get_node_or_null("PromptPoint/InteractionLabel")
 
 var jugador_cerca: bool = false
 var completado: bool = false
@@ -20,6 +21,10 @@ func _ready() -> void:
 		if sprite_signo.has_method("stop"):
 			sprite_signo.stop()
 
+	if interaction_label:
+		interaction_label.visible = false
+		interaction_label.text = "Press E"
+
 func _process(_delta: float) -> void:
 	if jugador_cerca and not completado and Input.is_action_just_pressed("interact"):
 		interactuar()
@@ -27,6 +32,9 @@ func _process(_delta: float) -> void:
 func interactuar() -> void:
 	if GameManager.is_dialogue_active:
 		return
+
+	if interaction_label:
+		interaction_label.visible = false
 
 	var fue_correcto = GameManager.completar_boton_nivel_2(name)
 
@@ -42,10 +50,12 @@ func _on_body_entered(body: Node) -> void:
 	if body.is_in_group("player"):
 		jugador_cerca = true
 		mostrar_signo()
+		mostrar_label()
 
 func _on_body_exited(body: Node) -> void:
 	if body.is_in_group("player"):
 		jugador_cerca = false
+		ocultar_label()
 		if not completado:
 			ocultar_signo()
 
@@ -61,8 +71,17 @@ func ocultar_signo() -> void:
 		if sprite_signo.has_method("stop"):
 			sprite_signo.stop()
 
+func mostrar_label() -> void:
+	if interaction_label and not completado:
+		interaction_label.visible = true
+
+func ocultar_label() -> void:
+	if interaction_label:
+		interaction_label.visible = false
+
 func activar_correcto() -> void:
 	completado = true
+	ocultar_label()
 
 	if sprite_signo:
 		sprite_signo.visible = true
@@ -78,12 +97,17 @@ func activar_incorrecto() -> void:
 
 func mostrar_dialogo_error() -> void:
 	GameManager.is_dialogue_active = true
+	ocultar_label()
 	DialogueManager.show_dialogue_balloon(load(DIALOGO_ERROR))
 	await DialogueManager.dialogue_ended
 	GameManager.is_dialogue_active = false
 
+	if jugador_cerca and not completado:
+		mostrar_label()
+
 func mostrar_dialogo_completado() -> void:
 	GameManager.is_dialogue_active = true
+	ocultar_label()
 	DialogueManager.show_dialogue_balloon(load(DIALOGO_COMPLETE))
 	await DialogueManager.dialogue_ended
 	GameManager.is_dialogue_active = false
@@ -96,3 +120,5 @@ func reiniciar_boton() -> void:
 		sprite_signo.visible = false
 		if sprite_signo.has_method("stop"):
 			sprite_signo.stop()
+
+	ocultar_label()
